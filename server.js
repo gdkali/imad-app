f9aa784730cb725a9665ee438b0fc518ece0c709
Gagan Mishra <gaganmishr.17@gmail.com>
2018-02-28 17:34:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/ui/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
