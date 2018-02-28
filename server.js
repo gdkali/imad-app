2745e07d3da12d97caac3813429218a956994449
Gagan Mishra <gaganmishr.17@gmail.com>
2018-02-28 22:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,58 @@ app.get('/', function (req, res) {
 });
 
 
+var articleOne={
+    title:'Article By GD1',
+    heading:'Article One',
+    date:'1st March 2018',
+    content:`
+    <p>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <p>
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>`   
+ };
+ 
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmtemplate=`
+  <html>
+    <head>
+        <title>${title}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="/ui/style.css" rel="stylesheet"/>
 
+    </head>
+    <body>
+        <div class=container>
+        <hr/>
+        <h3>${Article One}</h3>
+        <div>${date}</div>
+        <div>
+            ${content}
+<div>
+            <a href="http://gaganmishr17.imad.hasura-app.io">Home</a>
+        </div>
+
+        </div>
+        </div>
+    </body>
+</html>
+`;
+}
+  return htmltemplate;
+  
 app.get('/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
   
 app.get('/ui/article-one.html', function (req, res) {
-  res.sendFile(createTemplate(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/article-two.html', function (req, res) {
