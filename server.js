a8030143d831a3687d942a5c68d17514a9c11296
Gagan Mishra <gaganmishr.17@gmail.com>
2018-02-28 17:02:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'abc.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
