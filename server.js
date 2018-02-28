30452166e4d32a94e97dc1fd5f41e3c7b08e30c7
Gagan Mishra <gaganmishr.17@gmail.com>
2018-02-28 17:33:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/article-one', function (req, res) {
+app.get('/ui/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
