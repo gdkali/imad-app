aa4a5ff4b80de70526f3f2c617aaec06c83fa1a4
Gagan Mishra <gaganmishr.17@gmail.com>
2018-02-28 21:46:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,30 +21,7 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 Why do we use it?
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>`   
 }
-var htmlTemp={
-    <html>
-    <head>
-        <title>${title}</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="/ui/style.css" rel="stylesheet"/>
 
-    </head>
-    <body>
-        <div class=container>
-        <hr/>
-        <h3>${content}</h3>
-        <div>${content}</div>
-        <div>
-            ${content}
-<div>
-            <a href="http://gaganmishr17.imad.hasura-app.io">Home</a>
-        </div>
-
-        </div>
-        </div>
-    </body>
-</html>`
-}
 
 app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
