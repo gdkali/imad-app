f495ae334afdbc18454e20227494139146e3cb80
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-01 15:15:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,18 @@
 console.log('Loaded!');
 alert('Hello Welcome To My WEBAPP');
 
-//change the txext
-var element =document.getElementById('main-text')
-;
-element.innerHTML ="New Value";
+//counter code
+var button=document.getElementById('counter');
+var counter =0;
 
-//mmove image
-
-var img = document.getElementById('abc');
-var marginLeft =0;
-function moveRight(){
-      marginLeft=marginLeft +1;
-      img.style.marginLeft =marginLeft +"px";
+button.onclick =function(){
+    
+    //make a request to the counter
+    
+    //capture response
+    
+    //render variable
+    counter=counter +1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
 }
-img.onclick =function () {
-    var interval =setInterval(moveRight,50);
-};
