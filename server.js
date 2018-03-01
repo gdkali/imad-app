bbf02851494ae00aa59b682a6fe2e11f6e8706a3
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-01 15:10:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ return ht;
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
 app.get('/:articleName', function (req, res) {
   //articleName == article-one
   //articles[articleName] == {} content object for article one
