2e5d7086bee18992574105b5ccb4dbb54ef36ce8
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-01 14:32:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,6 @@ element.innerHTML ="New Value";
 
 var img=document.getElementById('madi');
 img.onclick =function(){
-    img.style.marginleft ='100px';
+    img.style.marginright ='100px';
     
 };
