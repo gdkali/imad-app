2c52cf454f9782013273c21fd60576c71f6692d6
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-05 16:54:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,21 @@ app.get('/:articleName', function (req, res) {
   res.send(xyz(articles[articleName]));
 });
 
-
+var pool= new Pool(config);
+ app.get('/test-db',function(req,res){
+     //make a request
+     //return a response with the results
+     pool.query('SELECT *FROM test',function(err,result){
+         if (err){
+             res.status(500).send(err.toString());
+         } else {
+             res.send(JSON.stringfy(result));
+         }     
+         
+     });
+     
+ });   
+ 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
