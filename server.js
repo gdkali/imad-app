d4fa1e399a0384cbecff4d69707db6d24611aeb7
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-08 16:47:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,13 +103,13 @@ app.get('/:articleName', function (req, res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkdf2Sync(input,salt,100,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
     return hashed.toString('hex');
 }
 
 app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'this is random string');
-     res.send(hasedString);
+     res.send(hashedString);
 }) ;
 
 
