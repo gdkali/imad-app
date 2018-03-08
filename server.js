270a2e35cbf362c72065977a3326c9be21479dd7
Gagan Mishra <gaganmishr.17@gmail.com>
2018-03-08 16:21:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool =require('pg').Pool;
+var Pool = require('pg').Pool;
+var crypto
 
 var config = {
     user:'gaganmishr17',
